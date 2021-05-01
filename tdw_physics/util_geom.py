71dfd05d450dec6cf3b9413d9814185bcdd4c325
fishtung <htung@andrew.cmu.edu>
2021-05-01 00:44:04 -0400

    remove maptlotlib, trimesh

--- tdw_physics/util_geom.py
+++ tdw_physics/util_geom.py
@@ -1,9 +1,6 @@
 import numpy as np
 import os
-import trimesh
 import copy
-import matplotlib.pyplot as plt
-
 
 
 def save_obj(vertices: np.ndarray, faces: np.ndarray, filepath: str):
@@ -16,25 +13,6 @@ def save_obj(vertices: np.ndarray, faces: np.ndarray, filepath: str):
             for vertex in face:
                 f.write(" %d" % (vertex + 1))
             f.write("\n")
-
-
-def as_mesh(scene_or_mesh):
-    """
-    Convert a possible scene to a mesh.
-    If conversion occurs, the returned mesh has only vertex and face data.
-    """
-    if isinstance(scene_or_mesh, trimesh.Scene):
-        if len(scene_or_mesh.geometry) == 0:
-            mesh = None  # empty scene
-        else:
-            # we lose texture information here
-            mesh = trimesh.util.concatenate(
-                tuple(trimesh.Trimesh(vertices=g.vertices, faces=g.faces)
-                    for g in scene_or_mesh.geometry.values()))
-    else:
-        assert(isinstance(scene_or_mesh, trimesh.Trimesh))
-        mesh = scene_or_mesh
-    return mesh
 
 
 
