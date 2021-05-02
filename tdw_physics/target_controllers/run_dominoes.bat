80a54ceb14394267d0893267ef20bea787285567
Elias Wang <eliaszwang@outlook.com>
2021-05-01 21:41:50 -0700

    update bat file

--- tdw_physics/target_controllers/run_dominoes.bat
+++ tdw_physics/target_controllers/run_dominoes.bat
@@ -17,10 +17,8 @@ for %%a in (val test train) do (
    set groups[!i!]=%%a
 )
 
-FOR /L %%i in (1,1,3) do (        
+FOR /L %%i in (1,1,%i%) do (        
     FOR /D %%d in (c:/Users/eliwang/human-physics-benchmarking/stimuli/generation/pilot-dominoes/*) DO (
-        C:/Users/eliwang/.conda/envs/tdw/python.exe c:/Users/eliwang/tdw_physics/tdw_physics/target_controllers/dominoes.py @c:/Users/eliwang/human-physics-benchmarking/stimuli/generation/pilot-dominoes/%%~nd/commandline_args.txt --training_data_mode --dir "Z:/eliwang/dominoes/%%~nd/!groups[%%i]!" --port 1071 --save_passes "" --height 256 --width 256 --monochrome 0 --random 0 --seed !seeds[%%i]! --num !nums[%%i]!
+        START /B /wait C:/Users/eliwang/.conda/envs/tdw/python.exe c:/Users/eliwang/tdw_physics/tdw_physics/target_controllers/dominoes.py @c:/Users/eliwang/human-physics-benchmarking/stimuli/generation/pilot-dominoes/%%~nd/commandline_args.txt --training_data_mode --dir "Z:/eliwang/dominoes/%%~nd/!groups[%%i]!" --port 1071 --height 256 --width 256 --monochrome 0 --random 0 --seed !seeds[%%i]! --num !nums[%%i]! --save_passes ''
     )
 )
-
-REM C:/Users/eliwang/.conda/envs/tdw/python.exe c:/Users/eliwang/tdw_physics/tdw_physics/target_controllers/dominoes.py @c:/Users/eliwang/human-physics-benchmarking/stimuli/generation/pilot-dominoes/pilot_dominoes_0mid_d3chairs_o1plants_tdwroom/commandline_args.txt --training_data_mode --dir "Z:/eliwang/dominoes/pilot_dominoes_0mid_d3chairs_o1plants_tdwroom" --port 1071 --save_passes "" --height 256 --width 256 --monochrome 0 --random 0 --seed 0 --num 15 
