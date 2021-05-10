c61509c590f920bc5e038236bf6832849d7a3ac5
Fish Tung <sfish0101@gmail.com>
2021-05-10 05:08:08 +0100

    adding save meshes for several scenarios

--- tdw_physics/target_controllers/linking.py
+++ tdw_physics/target_controllers/linking.py
@@ -693,7 +693,8 @@ if __name__ == "__main__":
                write_passes=args.write_passes.split(','),               
                save_passes=args.save_passes.split(','),
                save_movies=args.save_movies,
-               save_labels=args.save_labels,               
+               save_labels=args.save_labels,
+               save_meshes=args.save_meshes,          
                args_dict=vars(args)
         )
     else:
