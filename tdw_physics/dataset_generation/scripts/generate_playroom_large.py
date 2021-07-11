a34fec6256d5c8d09084befe0f1b3e3c540f71a0
Daniel Bear <dbear9887@gmail.com>
2021-07-11 13:02:58 -0400

    model splits

--- tdw_physics/dataset_generation/scripts/generate_playroom_large.py
+++ tdw_physics/dataset_generation/scripts/generate_playroom_large.py
@@ -43,7 +43,16 @@ def get_args(dataset_dir: str):
     parser.add_argument("--num_static_models",
                         type=int,
                         default=NUM_STATIC_MODELS,
-                        help="number of models that will be static")        
+                        help="number of models that will be static")
+    parser.add_argument("--num_trials_per_model",
+                        type=int,
+                        default=10,
+                        help="Number of trials to create per moving model")
+    parser.add_argument("--split",
+                        type=int,
+                        default=-1,
+                        help="Which split of the trials to generate")
+                        
 
     args = parser.parse_args()
     args = playroom_postproc(args)
@@ -78,31 +87,76 @@ def make_category_splits(categories=CATEGORIES, num_per_split=200, seed=0):
 
 def split_models(category_splits, num_models_per_split=[1000,1000], seed=0):
 
-    rng = np.random.RandomState(seed)
+    rng = np.random.RandomState(seed=seed)
     model_splits = OrderedDict()
     cat_split_ind = 0
     for i,num in enumerate(num_models_per_split):
         models_here = []        
         while len(models_here) < num:
             cats = category_splits[cat_split_ind]
-            for cat in cats:
+            for cat in sorted(cats):
                 models_here.extend(MODELS_PER_CATEGORY[cat])
             cat_split_ind += 1
-        model_splits[i] = rng.permutation([r.name for r in models_here])[:num]
+
+        model_splits[i] = rng.permutation(sorted([r.name for r in models_here]))[:num]
 
     return model_splits
+
+def build_scenarios(moving_models, static_models, num_trials_per_model, seed=0):
+
+    rng = np.random.RandomState(seed=seed)
+    num = num_trials_per_model
+    NM = len(moving_models)
+    NS = len(static_models)
+
+    probes = rng.permutation(moving_models)
+    targets = rng.permutation(moving_models)
+    distractors = rng.permutation(static_models)    
+    occluders = rng.permutation(static_models)
+
+    scenarios = []
+    for i in range(num * NM):
+        probe_ind = i // num
+        target_ind = i % NM
+        dist_ind = occ_ind = i % NS
+        scene = (
+            probes[probe_ind],
+            targets[target_ind],
+            distractors[dist_ind],
+            occluders[occ_ind]
+        )
+        scenarios.append(scene)
+
+    return scenarios
+    
+
+def build_controller(args):
+
+    C = Playroom(
+    )
+
+    return C
 
 def main(args):
     category_splits = make_category_splits(
         seed=args.category_seed,
         num_per_split=args.models_per_split,
     )
-    model_splits = split_models(category_splits, [args.num_moving_models, args.num_static_models],
+
+    mps = args.models_per_split
+    num_moving_splits = args.num_moving_models // mps
+    num_static_splits = args.num_static_models // mps
+    nums_per_split = [mps] * (num_moving_splits + num_static_splits)
+    model_splits = split_models(category_splits, nums_per_split,
                                 seed=args.category_seed)
+    moving_splits = [model_splits[i] for i in range(num_moving_splits)]
+    static_splits = [model_splits[j] for j in range(num_moving_splits, num_moving_splits + num_static_splits)]
+    all_static_models = []
+    for s in static_splits:
+        all_static_models.extend(s)
 
-    print([len(model_splits[i]) for i in [0,1]])
-
-    
+    # print(len(moving_splits[0]), len(static_splits[0]), len(all_static_models))
+    print(moving_splits[args.split], len(moving_splits[args.split]))
 
 if __name__ == '__main__':
 
