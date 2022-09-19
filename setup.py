0405111adb7b39214a3c54237dbb5e362424bb60
kasmith <k2smith@mit.edu>
2022-09-19 10:20:48 -0400

    Updated noisy physics structure

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
                       'pillow',
                       'weighted-collection',
                       'stopit',
-                      'tdw==1.8.29',
+                      'tdw>=1.9.16',
                       'matplotlib',
                       'trimesh'],
 )
